b1214c6844f3313be173676a3a2add13c41801ce
KehindeBello <adedotunomomeji@gmail.com>
2022-09-29 08:21:53 +0100

    to-use travis env

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// function sleep(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
 const favoriteFood = process.env.FAVORITE_FOOD;
-console.log(`My favorite food is ${favoriteFood}`);
 
-// async function main() {
-//   while(true) {
-//     console.log('Containers rule!');
-//     await sleep(5000);
-//   }
-// }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// main();
+async function main() {
+  while(true) {
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
