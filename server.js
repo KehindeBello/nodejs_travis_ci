2f1019cc12a24805c9c07c042ba7c9d9316e0f2f
KehindeBello <adedotunomomeji@gmail.com>
2022-09-29 08:11:19 +0100

    try travis env

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
-async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
-  }
-}
+const favoriteFood = process.env.FAVORITE_FOOD;
+console.log(`My favorite food is ${favoriteFood}`);
 
-main();
+// async function main() {
+//   while(true) {
+//     console.log('Containers rule!');
+//     await sleep(5000);
+//   }
+// }
+
+// main();
